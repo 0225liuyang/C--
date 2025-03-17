fdf1623db556de6b359c32992c0aff3f81912d87
0225liuyang <318633918@qq.com>
2025-03-17 23:44:04 +0800

    fourth commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+<<<<<<< HEAD
     string s;
     cin>>s;
+=======
+>>>>>>> parent of 79e2287 (second commit)
     cout<<"hello world"<<endl;
     return 0;
 }
